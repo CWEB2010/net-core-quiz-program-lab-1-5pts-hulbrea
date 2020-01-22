04a8023e30177f9055184735d8bebec2bb01dd19
hulbrea <hulbrea@dunwoody.edu>
2020-01-22 14:34:24 -0600

    added comment to test push functionality

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -7,6 +7,7 @@ namespace lab_one
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //here is some new code that I added to test the git push functionality
         }
     }
 }
