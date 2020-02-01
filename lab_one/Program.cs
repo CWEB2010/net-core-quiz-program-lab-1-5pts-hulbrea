b2e4de05aed8bd0f8147ed9484350191a7b22f7e
Hull <hulbrea@dunwoody.edu>
2020-01-31 18:52:52 -0600

    Named the variables and created the intro message. Questions still need to be made.

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Brenna Hull
+//CWEB2010 Advanced Programming
+//Lab 1: Make a quiz over .NET
+
+using System;
 
 namespace lab_one
 {
@@ -6,8 +10,35 @@ namespace lab_one
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //here is some new code that I added to test the git push functionality
+            string[] answerKey = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
+            string[] userchoices;
+            string[] userSelection;
+            string[] questions = { "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10" };
+            
+            
+            // Two dimensional array with possible question answers
+            string[,] optionChoices = {
+                                        {"A", "B", "C", "D"},
+                                        {"A", "B", "C", "D"},
+                                        {"A", "B", "C", "D"},
+                                        {"A", "B", "C", "D"},
+
+            };
+
+            int primingValue; 
+            int EXIT = 2; //Sentenial Value
+            int x, y;
+            int totalCorrect, totalIncorrect;
+
+            Console.WriteLine("Welcome, this is a quiz on .NET Core. This quiz will consist of 10 questions. You must get 7 of the 10 correct to pass.");
+            Console.WriteLine("To get started, enter the number '1'.");
+
+            primingValue = Console.Read();
+
+        
+
+
         }
+    }
     }
 }
