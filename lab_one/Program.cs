0d28b8c8008ae6a738456f4095c200d76272ca27
Hull <hulbrea@dunwoody.edu>
2020-02-02 19:52:28 -0600

    Finished the coding. Test for bugs next

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -12,30 +12,28 @@ namespace lab_one
         {
 
             string[] answerKey = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
-            string userChoices;
-            string userSelection;
-            string[] questions = { "What will be replacing .NET Core in 2020?", "Q2", "What is the latest version of .NET Core?", "Q4", "What platforms does .NET Core support?", "Q6", "Q7", "Q8", "Who maintains .NET Core?", "Q10" };
+            string[] userChoices = { };
+            string[] userSelection;
+            string[] questions = { "What will be replacing .NET Core in 2020?", "What is the latest version of .NET Core?", "What platforms does .NET Core support?", "Who maintains .NET Core?"};
             
             
             // Two dimensional array with possible question answers
             string[,] optionChoices = {
                                         {"A. .NET Framework", "B. .NET 5", "C. .NET Applications", "D. There's nothing new coming"},
-                                        {"A.", "B.", "C.", "D."},
+                                        
                                         {"A. 1.0", "B. 2.0", "C. 3.0", "D. 4.0"},
-                                        {"A. ", "B", "C.", "D"},
+                                    
                                         { "A. Windows", "B. macOS", "C. Linux", "D. All of the above" },
-                                        {"A", "B", "C", "D"},
-                                        {"A", "B", "C", "D"},
+                                        
                                         {"A. IBM", "B. Microsoft", "C. .NET community of GitHub", "D. B and C"},
-                                        {"A", "B", "C", "D"},
+                                      
             };
 
             int primingValue; 
             int EXIT = 2; //Sentenial Value
-            
-            int totalCorrect, totalIncorrect;
 
-            Console.WriteLine("Welcome, this is a quiz on .NET Core. This quiz will consist of 10 questions. You must get 7 of the 10 correct to pass.");
+
+            Console.WriteLine("Welcome, this is a quiz on .NET Core. This quiz will consist of 3 questions. You must get 2 of the 3 correct to pass.");
             Console.WriteLine("To get started, enter the number '1'.");
 
             primingValue = Console.Read();
@@ -56,7 +54,7 @@ namespace lab_one
                     totalCorrect = 0;
                     totalIncorrect = 0;
 
-                    if (userSelection == answerKey[x])
+                    if (userSelection = answerKey[x])
                     {
                         totalCorrect++;
                         Console.WriteLine("You got it right!");
@@ -68,23 +66,25 @@ namespace lab_one
                     }
                     userChoices[x] = userSelection;
                 }
-            Console.WriteLine("The quiz has been conclued, your results are listed below.");
-            Console.WriteLine("Total Correct: " + totalCorrect);
-            Console.WriteLine("Total Incorrect: " + totalIncorrect);
+                int totalCorrect;
+                int totalIncorrect;
+                Console.WriteLine("The quiz has been conclued, your results are listed below.");
+                Console.WriteLine("Total Correct: " + totalCorrect);
+                Console.WriteLine("Total Incorrect: " + totalIncorrect);
 
-            if (totalCorrect >= 2)
-            {
-                Console.WriteLine("You passed the quiz");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, you failed, please retake the quiz");
+                if (totalCorrect >= 2)
+                {
+                    Console.WriteLine("You passed the quiz");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you failed, please retake the quiz");
 
-            }
-            Console.WriteLine("To retake the quiz, please enter 1. Enter 2 to exit the quiz.");
-            primingValue = Console.Read();
-            }
-            Console.WriteLine("Thank you for taking the .NET Core quiz";
+                }
+                Console.WriteLine("To retake the quiz, please enter 1. Enter 2 to exit the quiz.");
+                primingValue = Console.Read();
+                }
+                Console.WriteLine("Thank you for taking the .NET Core quiz");
 
         }
     }
